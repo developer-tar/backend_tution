a61cd5311dc374c5406b942b575bcb51615a4a7e
tarun kumar <tarunkumar@gmail.com>
2025-05-27 08:52:09 +0530

    pusdh

--- database/migrations/2025_05_26_181820_create_student_details_table.php
+++ database/migrations/2025_05_26_181820_create_student_details_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('student_details', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->for
             $table->timestamps();
         });
     }
