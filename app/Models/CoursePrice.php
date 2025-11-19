c81d4c369b6f3e03976a0eec5037316993cf5ad9
tarun kumar <tarunkumar@gmail.com>
2025-11-19 23:26:49 +0530

    add the softdelete and migration

--- app/Models/CoursePrice.php
+++ app/Models/CoursePrice.php
@@ -3,9 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class CoursePrice extends Model
 {
+    use SoftDeletes;
     protected $fillable = [
         'course_id',
         'billing_period_id',
